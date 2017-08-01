3b4973ae44209b53c5adc90b7c43392073639fd3
Timothy Stelter <tstelter@vt.edu>
2017-08-01 16:54:02 -0400

    - Updated the constants file to include a static final map of the usable algorithms. - changed the constructor from 'public' to 'private'

--- Algorithms/src/Constants.java
+++ Algorithms/src/Constants.java
@@ -1,18 +1,33 @@
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Hashtable;
+import java.util.Map;
+
 /**
  * 
- * Constants file
+ * Constants file.  We don't want magic numbers.
  * 
  * @author Timothy Stelter
- *
+ * @version v8117
  */
 public final class Constants {
     
-	public Constants() {
+	private Constants() {
 		/*Nothing to instantiate*/
 	}
 
 	/*Public constant(s)*/
-	public static final int ARRAY_SIZE_MAX_LIMIT = 10000000;
+	public static final int ZERO                 = 0;
 	public static final int ARRAY_SIZE_MIN_LIMIT = 2;
+	public static final int ARRAY_SIZE_MAX_LIMIT = 10000000;
+	public static final Map<Integer, String> ALGO_MAP;
+    static{
+        Hashtable<Integer,String> tmp = 
+            new Hashtable<Integer,String>();
+        tmp.put(1,"Bubble Sort");
+        tmp.put(2,"Insertion Sort");
+        tmp.put(3,"Merge Sort");
+        ALGO_MAP = Collections.unmodifiableMap(tmp);
+    }
 	
 }
