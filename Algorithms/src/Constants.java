efcf469aa4a5cc192d821605628c9305102a2077
Timothy Stelter <tstelter@vt.edu>
2017-08-01 16:31:23 -0400

    - Adding Constants file for good practice

---
+++ Algorithms/src/Constants.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ * Constants file
+ * 
+ * @author Timothy Stelter
+ *
+ */
+public final class Constants {
+    
+	public Constants() {
+		/*Nothing to instantiate*/
+	}
+
+	/*Public constant(s)*/
+	public static final int ARRAY_SIZE_MAX_LIMIT = 10000000;
+	public static final int ARRAY_SIZE_MIN_LIMIT = 2;
+	
+}
