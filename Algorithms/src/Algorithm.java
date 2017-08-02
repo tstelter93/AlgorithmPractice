42542859c634f838a7bb4563195516293b1509df
Timothy Stelter <tstelter@vt.edu>
2017-08-02 15:55:22 -0400

    - Added Abstract algorithm class to link together all algorithm classes to a super

---
+++ Algorithms/src/Algorithm.java
@@ -0,0 +1,8 @@
+/**
+ * Abstract template class for algorithm classes
+ * @author Timothy Stelter
+ */
+public abstract class Algorithm {
+    /*Leave blank.  Used for connection the algorithms together.*/
+	abstract String getName();
+}
